7b147072e1837dc152dea79225e68c5b092f3e39
krawieccezary <32022036+krawieccezary@users.noreply.github.com>
2018-01-11 22:33:35 +0100

    add remove

--- main.js
+++ main.js
@@ -1,40 +1,43 @@
 var main = function () {
-    $('.alert').hide();
+        $('.alert').hide();
 
-    $('#add').click(function () {
-        var zadanie = $('#task').val();
+        $('#add').click(function () {
+                var zadanie = $('#task').val();
 
-        console.log(1);
+                console.log(1);
 
-        if (zadanie.length == 0) {
-            $('.alert').show();
-        } else {
+                if (zadanie.length == 0) {
+                    $('.alert').show();
+                } else {
 
-            var tick_button = $('<img class="buttons" id="tick_button" width="70" height="70" src="images/tick.png"/>');
+                    var tick_button = $('<img class="buttons" id="tick_button" width="70" height="70" src="images/tick.png"/>');
 
-            var edit_button = $('<img class="buttons" id="edit_button" width="70" height="70" src="images/edit.png"/>');
+                    var edit_button = $('<img class="buttons" id="edit_button" width="70" height="70" src="images/edit.png"/>');
 
-            let remove_button = $('<button id="remove"></button>');
+                     var remove_button = $('<img class="buttons" id="remove" width="70" height="70" src="images/remove.png"/>');
+                    $('<li>').text(zadanie).prependTo('#list').addClass('task');
+                    $('#list :nth-child(1)').append(tick_button).append(edit_button).append(remove_button);
 
-            $('<li>').text(zadanie).prependTo('#list').addClass('task');
-            $('#list :nth-child(1)').append(tick_button).append(edit_button).append(remove_button);
+                    $('#task').val('');
+                    $('.alert').hide();
 
-            $('#task').val('');
-            $('.alert').hide();
-        }
-    });
+                    $('#list').click(function(event) {
 
-    $('#remove').click(function () {
-        // var remove_parent = this.parentNode;
+                        
+                            if (event.which = '#remove')   {
+                                var parent = event.target.parentNode;
+                                 $(parent).remove();
+                                  console.log(parent); } 
+                            });
+                          
 
-        console.log(5);
-
-    });
-
-    /*  if (number == 0) 
-           $('<h2>').text('Brak zadań!').after('#menu');
-      else $('h2').remove(); */
-}
+                        };
+                    });
+                };
 
 
-$(document).ready(main);
+
+            /*  if (number == 0) 
+                   $('<h2>').text('Brak zadań!').after('#menu');
+              else $('h2').remove(); */
+        $(document).ready(main);
