804db953daee42e80b8b02be43d777dc48caaefe
krawieccezary <32022036+krawieccezary@users.noreply.github.com>
2018-01-16 19:46:19 +0100

    add event

--- main.js
+++ main.js
@@ -10,34 +10,54 @@ var main = function () {
                     $('.alert').show();
                 } else {
 
-                    var tick_button = $('<img class="buttons" id="tick_button" width="70" height="70" src="images/tick.png"/>');
+                    var tick_button = $('<img class="buttons" id="tick" width="70" height="70" src="images/tick.png"/>');
 
-                    var edit_button = $('<img class="buttons" id="edit_button" width="70" height="70" src="images/edit.png"/>');
+                    var edit_button = $('<img class="buttons" id="edit" width="70" height="70" src="images/edit.png"/>');
 
-                     var remove_button = $('<img class="buttons" id="remove" width="70" height="70" src="images/remove.png"/>');
+                    var remove_button = $('<img class="buttons" id="remove" width="70" height="70" src="images/remove.png"/>');
                     $('<li>').text(zadanie).prependTo('#list').addClass('task');
                     $('#list :nth-child(1)').append(tick_button).append(edit_button).append(remove_button);
 
                     $('#task').val('');
                     $('.alert').hide();
 
-                    $('#list').click(function(event) {
+                    $('#list').click(function (event) {
+                         var parent = event.target.parentNode;
 
+                        if (event.which = '#remove') {
+                            $(parent).remove();
+                            console.log(parent);
+                        }
+
+
+                        if (event.which = '#edit') {
+                            
+                             console.log('edit');
+                          /*  $('.selected').html(function () {
+
+                                return '<input type="text" name="editt_place" id="edit_place" placeholder="' + $(this).text() + '"/>'; */
+
+                           
+                           // };
                         
-                            if (event.which = '#remove')   {
-                                var parent = event.target.parentNode;
-                                 $(parent).remove();
-                                  console.log(parent); } 
-                            });
-                          
+                        $('#edit_place').keypress(function (e) {
+                            if (e.which === 13) {
+                                var edit_text = $('#edit_place').val();
+                                $('.selected').text(edit_text).prependTo('#list').toggleClass('deselected');
 
-                        };
-                    });
-                };
+                                console.log(edit_text);
+                            }
+                        });
+                         };
+                    
+
+                });
+        };
 
 
-
-            /*  if (number == 0) 
-                   $('<h2>').text('Brak zadań!').after('#menu');
-              else $('h2').remove(); */
+        });
+};
+        /*  if (number == 0) 
+               $('<h2>').text('Brak zadań!').after('#menu');
+          else $('h2').remove(); */
         $(document).ready(main);
